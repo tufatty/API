1877af9c9059cb363f4475db2e214f1a57938e91
tufatty <ugonjokubarthlomew@gmail.com>
2025-10-17 06:22:53 +0000

    greatness

--- profileapi/profileapi/wsgi.py
+++ profileapi/profileapi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proflieapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profileapi.settings')
 
 application = get_wsgi_application() 
